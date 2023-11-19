62906ba19c67b2ef547918ceca28fb107535274a
osparking <jbpark03@gmail.com>
2023-11-19 15:53:41 +0900

    세개의 assume* 메소드 사용 실습

---
+++ src/test/java/assumptions/AssumptionsTest.java
@@ -0,0 +1,42 @@
+package assumptions;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import assumptions.environment.JavaSpecification;
+import assumptions.environment.OperationSystem;
+import assumptions.environment.TestsEnvironment;
+
+class AssumptionsTest {
+	private static String JAVA_VERSION = "1.8";
+	private TestsEnvironment env = new TestsEnvironment(
+			new JavaSpecification(
+					System.getProperty("java.vm.specification.version")),
+			new OperationSystem(System.getProperty("os.name"),
+					System.getProperty("os.arch")));
+
+	private SUT systemUnderTest = new SUT();
+
+	@BeforeEach
+	void setUp() {
+		assumeTrue(env.isWindows());
+	}
+
+	@Test
+	void testNoJobToRun() {
+		assumingThat(() -> env.getJavaVersion().equals(JAVA_VERSION),
+				() -> assertFalse(systemUnderTest.hasJobToRun()));
+	}
+
+	@Test
+	void testJobToRun() {
+		assumeTrue(env.isAmd64Architecture());
+		systemUnderTest.run(new Job());
+		assertTrue(systemUnderTest.hasJobToRun());
+	}
+}
