bcffe80c9b964c777c7c25b21bf61db7a22f91bd
osparking <jbpark03@gmail.com>
2023-11-19 10:25:43 +0900

    시험 기한 초과 작업 조기 종료 시험 사례

--- src/test/java/assertions/AssertTimeoutTest.java
+++ src/test/java/assertions/AssertTimeoutTest.java
@@ -1,6 +1,7 @@
 package assertions;
 
 import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,14 @@ import static java.time.Duration.ofMillis;
 
 public class AssertTimeoutTest {
 	private SUT systemUnderTest = new SUT("시험될 우리 시스템");
+	
+	@Test
+	@DisplayName("조기 종료 가능성 있었으나, 500ms 내 작업 성공적으로 종료함")
+	void testTimeoutPreemptively() throws InterruptedException {
+		systemUnderTest.addJob(new Job("A Job that takes more than 500ms"));
+		assertTimeoutPreemptively(ofMillis(500), 
+				() -> systemUnderTest.run(600));
+	}
 	
 	@Test
 	@DisplayName("500ms 내에 작업이 수행됨")
