4efed88a01e4386be58e0f97cc4bcebc03b758ca
osparking <jbpark03@gmail.com>
2023-11-19 09:26:54 +0900

    공급기를 사용한 메시지 생성비용 절약 - 2탄

--- src/test/java/assertions/AssertAllTest.java
+++ src/test/java/assertions/AssertAllTest.java
@@ -6,6 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AssertAllTest {
+	String systemName = "검증 대상 우리 시스템";
+	SUT systemUnderTest = new SUT(systemName);
+	
+	@Test
+	@DisplayName("시스템은 현재 검증되고 있지 않다.")
+	void testSysNotUnderVerif() {
+		assertFalse(systemUnderTest.isVerified(),
+				() -> "'" + systemName +"'은 현재 검증되고 있지 않아야 함.");
+	}
 	
 	@Test
 	@DisplayName("SUT는 현재 검증되고 있어야 함")
