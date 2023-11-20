23532540300387c195e8c46d8e34a4c41c9f3f21
osparking <jbpark03@gmail.com>
2023-11-20 09:18:00 +0900

    컴파일 오류 수정 - 수입 구성을 통함

--- src/test/java/disabled/DisabledTestMethods.java
+++ src/test/java/disabled/DisabledTestMethods.java
@@ -1,7 +1,7 @@
 package disabled;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
