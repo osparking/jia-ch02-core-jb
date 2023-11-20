84088aec189fcc1240529bebbb3fe0bb60352305
osparking <jbpark03@gmail.com>
2023-11-20 15:16:49 +0900

    RepetitionInfo 인자로 반복 행번호 출력

--- src/test/java/repeated/RepeatedTestTest.java
+++ src/test/java/repeated/RepeatedTestTest.java
@@ -2,8 +2,15 @@ package repeated;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestReporter;
 
 class RepeatedTestTest {
 	
@@ -15,4 +22,21 @@ class RepeatedTestTest {
 		assertEquals(2, calculator.add(1, 1), "1 + 1 은 2 이다.");
 	}
 
+	private static Set<Integer> integerSet = new HashSet<>();
+	private static List<Integer> integerList = new ArrayList<>();
+	
+	@RepeatedTest(value =5, 
+			name = "Current line #: {currentRepetition}, Set element count: 1" )
+	void testAddingToCollections(TestReporter testReporter, 
+			RepetitionInfo repetitionInfo) {
+		integerSet.add(1);
+		integerList.add(repetitionInfo.getCurrentRepetition());
+		
+		testReporter.publishEntry("Repetition odrer", 
+				String.valueOf(repetitionInfo.getCurrentRepetition()));
+		
+		assertEquals(1, integerSet.size());
+		assertEquals(repetitionInfo.getCurrentRepetition(), integerList.size());
+		
+	}
 }
